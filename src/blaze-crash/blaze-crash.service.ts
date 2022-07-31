7de1db9de059106306c8a29ff6fe0ae470004927
Renato Santos <renato.santos0611@gmail.com>
2022-07-31 14:04:51 -0300

    Added headers axios request

--- src/blaze-crash/blaze-crash.service.ts
+++ src/blaze-crash/blaze-crash.service.ts
@@ -12,8 +12,13 @@ export class BlazeCrashService {
 
   @Cron('*/10 * * * * *')
   async getBlazeCrash() {
+    const headersRequest = {
+      'Content-Type': 'application/json', // afaik this one is not needed
+      'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIwOTQ5NzgsImJsb2NrcyI6W10sImlhdCI6MTY1ODc4Mzk1MywiZXhwIjoxNjYzOTY3OTUzfQ.L-Ftt7gqU2vpowIWcTWSrxoDMzxc5VFg4B-lJaXb_40`,
+    };
     const request = this.httpService.get(
       'https://blaze.com/api/crash_games/recent/history',
+      { headers: headersRequest },
     );
     const blazeCrashRecords = await lastValueFrom(request);
     const blazeCrashProps = blazeCrashRecords.data.records;
