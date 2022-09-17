f62aeac212af1d201e326959f27a95c97576d157
Renato Santos <renato.santos0611@gmail.com>
2022-09-17 08:43:53 -0300

    Change blaze Api Link

--- src/blaze-crash/blaze-crash.service.ts
+++ src/blaze-crash/blaze-crash.service.ts
@@ -14,18 +14,22 @@ export class BlazeCrashService {
   async getBlazeCrash() {
     const headersRequest = {
       'Content-Type': 'application/json', // afaik this one is not needed
-      'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIwOTQ5NzgsImJsb2NrcyI6W10sImlhdCI6MTY1ODc4Mzk1MywiZXhwIjoxNjYzOTY3OTUzfQ.L-Ftt7gqU2vpowIWcTWSrxoDMzxc5VFg4B-lJaXb_40`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIwOTQ5NzgsImJsb2NrcyI6W10sImlhdCI6MTY1ODc4Mzk1MywiZXhwIjoxNjYzOTY3OTUzfQ.L-Ftt7gqU2vpowIWcTWSrxoDMzxc5VFg4B-lJaXb_40`,
     };
     const request = this.httpService.get(
-      'https://blaze.com/api/crash_games/recent/history',
-      { headers: headersRequest },
+      'https://api-v2.blaze.com/crash_games/recent/history',
+      {
+        headers: headersRequest,
+      },
     );
     const blazeCrashRecords = await lastValueFrom(request);
     const blazeCrashProps = blazeCrashRecords.data.records;
-    for(const crash in blazeCrashProps) {
-      if(!this.records.find(record => record.id === blazeCrashProps[crash].id)) {
-          const record = new BlazeCrashBulder(blazeCrashProps[crash]);
-          this.records.push(record.getObject());
+    for (const crash in blazeCrashProps) {
+      if (
+        !this.records.find((record) => record.id === blazeCrashProps[crash].id)
+      ) {
+        const record = new BlazeCrashBulder(blazeCrashProps[crash]);
+        this.records.push(record.getObject());
       }
     }
   }
@@ -38,5 +42,4 @@ export class BlazeCrashService {
   cleanRecords() {
     this.records = this.records.slice(-1000);
   }
-
 }
